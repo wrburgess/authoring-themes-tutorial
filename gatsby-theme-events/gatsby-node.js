030a0206b0286a2e3c7519f5b3d3b2593c407c50
Randy Burgess <wrburgess@gmail.com>
2019-12-12 17:07:36 -0600

    initial commit

---
+++ gatsby-theme-events/gatsby-node.js
@@ -0,0 +1,80 @@
+const fs = require("fs")
+
+exports.onPreBootstrap =({ reporter }) => {
+  const contentPath = "data"
+  if (!fs.existsSync(contentPath)) {
+    reporter.info(`creating ${contentPath} directory`)
+    fs.mkdirSync(contentPath)
+  }
+}
+
+exports.sourceNodes = ({ actions }) => {
+  actions.createTypes(`
+    type Event implements Node @dontInfer {
+      id: ID!
+      name: String!
+      location: String!
+      startDate: Date! @dateformat @proxy(from: "start_date")
+      endDate: Date! @dateformat @proxy(from: "end_date")
+      url: String!
+      slug: String!
+    }
+  `)
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  const basePath = "/"
+
+  // Quick-and-dirty helper to convert strings into URL-friendly slugs.
+  const slugify = str => {
+    const slug = str
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/(^-|-$)+/g, "")
+
+    return `/${basePath}/${slug}`.replace(/\/\/+/g, "/")
+  }
+
+  createResolvers({
+    Event: {
+      slug: {
+        resolve: source => slugify(source.name),
+      },
+    },
+  })
+}
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const basePath = "/"
+  actions.createPage({
+    path: basePath,
+    component: require.resolve("./src/templates/events.js"),
+  })
+  const result = await graphql(`
+    query {
+      allEvent(sort: { fields: startDate, order: ASC }) {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `)
+
+  if(result.errors) {
+    reporter.panic("error loading events", result.errors)
+    return
+  }
+
+  const events = result.data.allEvent.nodes
+  events.forEach(event => {
+    const slug = event.slug
+    actions.createPath({
+      path: slug,
+      component: require.resolve("./src/templates/event.js"),
+      context: {
+        eventID: event.id,
+      },
+    })
+  })
+}
